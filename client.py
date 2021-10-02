bac823d100b34948288497551a8f3a7456ecb405
Krishna Kaipa <kaipakrishna380@gmail.com>
2021-10-03 01:05:42 +0400

    Added auth

--- client.py
+++ client.py
@@ -1,10 +1,51 @@
 #client program
 import socket
 import sys
-import threading
+import hashlib
 
 HOST = ''
 PORT = 6969
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST,PORT))
+def new_account(conn):
+    username = input("Enter username: ")
+    conn.send(username.encode())
+    password = input("Enter password: ")
+    conn.send(password.encode())
+    status = conn.recv(1024).decode()
+    if(status == "success"):
+        print("Account successfully created.")
+    elif(status == "fail"):
+        print("Account already exists.")
+
+def login(conn):
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    conn.send(username.encode())
+    rand_str = conn.recv(1024).decode()
+    auth_string = hashlib.md5((password+rand_str).encode())
+    conn.send(auth_string.hexdigest().encode())
+    status = conn.recv(1024).decode()
+    if(status == "success"):
+        print("Authentication successful.")
+    elif(status == "fail"):
+        print("Authentication failed.")
+
+def main():
+
+    conn = socket.socket()
+    conn.connect((HOST,PORT)) 
+    choice = input("1. Create account\n2. Login\n3. Exit\n Choice: ")
+    if choice == "1":
+        conn.send("new_account".encode())
+        new_account(conn)
+    elif choice == "2":
+        conn.send("login".encode())
+        login(conn)
+    elif choice == "3":
+        sys.exit()
+    else:
+        print("Invalid choice")
+
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,32 +1,114 @@
 #server program
 import socket
-import sys
+import os
+import hashlib
 import threading
+import sqlite3
+import random
+import string
 
 HOST = ''
 PORT = 6969
 
-def auth_user():
-    pass
+#function to generate random string
+def random_string():
+	return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+
+def create_user(db_conn,username,password):
+	cursor = db_conn.cursor()
+	cursor.execute('insert into usernames (name, password) values (?, ?)', (username, password,))
+	db_conn.commit()
+
+def fetch_user(db_conn,username):
+	cursor = db_conn.cursor() 
+	cursor.execute('select password from usernames where name = ?', (username,))
+	rows = cursor.fetchall()
+	return rows
+
+def new_account(conn,db_conn):
+	username = conn.recv(1024).decode()
+	password = conn.recv(1024).decode()
+	print(username,password)
+	print(fetch_user(db_conn,username))
+	if fetch_user(db_conn,username) == []:
+		create_user(db_conn,username,password)
+		conn.send("success".encode())
+	else:
+		conn.send("fail".encode())
+
+def login(conn,db_conn):
+
+	username = conn.recv(1024).decode()
+	user_data = fetch_user(db_conn,username)
+	password = user_data[0][0]
+	rand_str = random_string()
+	conn.send(rand_str.encode())
+	auth_string = hashlib.md5((password+rand_str).encode()).hexdigest()
+	challenge_response = conn.recv(1024).decode()
+	if auth_string == challenge_response:
+		conn.send("success".encode())
+	else:
+		conn.send("fail".encode())
+		conn.close()
 
 def on_connect(conn,addr):
-    with conn:
-        print('Connected by', addr)
+
+	database = "users.db"
+
+	db_conn = create_connection(database)
+	if db_conn is not None:
+	# create projects table
+		create_table(db_conn)
+		print("Table created.")
+	else:
+		print("Error! cannot create the database connection.")
+
+	print("Connected to database")
+	print('Connected by', addr)
+	choice = conn.recv(1024).decode()
+	if choice == "new_account":
+		new_account(conn,db_conn)
+	elif choice == "login":
+		login(conn,db_conn)
+	else:
+		return
+	
+def create_connection(db_file):
+
+	try:
+		db_conn = sqlite3.connect(db_file)
+		return db_conn
+	except:
+		print("Error in connection creation.")
+
+	return None
+
+def create_table(db_conn):
+
+	create_table_sql = """ CREATE TABLE IF NOT EXISTS usernames(
+										id integer primary key AUTOINCREMENT,
+										name nvarchar(40) not null,
+										password nvarchar(32) not null
+						); """
+	try:
+		c = db_conn.cursor()
+		c.execute(create_table_sql)
+	except:
+		print("Cannot Create Table")
 
 def main():
 
-    sock = socket.socket()
-    print("Socket successfully created")
-    port = 6969
-    sock.bind(('', port))
-    
-    while True:
-        sock.listen(5)
-        print("Listening for connections")
-        conn, addr = sock.accept()
-        threading.Thread(target=on_connect, args=(conn,addr,)).start()
-        print(".",end="")
+	sock = socket.socket()
+	print("Socket successfully created")
+	port = 6969
+	sock.bind(('', port))
+	print("Listening for connections")
+
+	while True:
+		sock.listen(5)    
+		conn, addr = sock.accept()
+		threading.Thread(target=on_connect, args=(conn,addr,)).start()
 
 if __name__ == "__main__":
-    main()
-    
+	main()
+	
