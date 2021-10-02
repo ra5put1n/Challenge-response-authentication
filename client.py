79f476a442c12d130b1270ee8e65002b35dd36a5
Krishna Kaipa <kaipakrishna380@gmail.com>
2021-10-02 17:33:50 +0400

    Boilerplate

--- client.py
+++ client.py
@@ -1,1 +1,10 @@
 #client program
+import socket
+import sys
+import threading
+
+HOST = ''
+PORT = 6969
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST,PORT))
--- server.py
+++ server.py
@@ -1,1 +1,32 @@
 #server program
+import socket
+import sys
+import threading
+
+HOST = ''
+PORT = 6969
+
+def auth_user():
+    pass
+
+def on_connect(conn,addr):
+    with conn:
+        print('Connected by', addr)
+
+def main():
+
+    sock = socket.socket()
+    print("Socket successfully created")
+    port = 6969
+    sock.bind(('', port))
+    
+    while True:
+        sock.listen(5)
+        print("Listening for connections")
+        conn, addr = sock.accept()
+        threading.Thread(target=on_connect, args=(conn,addr,)).start()
+        print(".",end="")
+
+if __name__ == "__main__":
+    main()
+    
